86062d5452f2cb6627bb2ab2db789cc087f6059e
wilodietch <wilodietch@gmail.com>
2020-09-18 23:45:11 +0000

    added variables

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import random
+
 print("**** Welcome to the Guessing Game! *****")
+
+#### Project Planning
+# 1) Number Randomizer for computer
+# 2) Loop for replaying
+# 3) Conditionals to play again or evaluate players number vs. computers number
+# 4) Provide user input for players number
+
+print("I am computer. Guess my number")
+
+computerNum = random.randint(0,20)
+print(computerNum)
+
+#state variables
+win = False
+still_playing = True
+tries = 5
+
+while still_playing == True:
+    while tries > 0:
+        print()
+        playerNum = int(input("Enter a number between 0-20:   "))
+        print(playerNum)
+
+
