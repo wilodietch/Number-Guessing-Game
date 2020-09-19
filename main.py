8140681d5629856e4ba79fdbe634f7866308585c
wilodietch <wilodietch@gmail.com>
2020-09-19 00:14:34 +0000

    tries

--- main.py
+++ main.py
@@ -33,18 +33,28 @@ while still_playing == True:
             tries -= 1
             print("You have " + str(tries) + " tries left. ")
         else:
-            print("You guessed the correct number")
+            print("You guessed the correct number!")
             win = True
             break
         if win == True:
-            print("Congratulations! You won at " + str(6-tries) + tries)
+            print("Congratulations! You won at " + str(6-tries) + " tries")
         else: 
             print("No more tries")
             print("The number was: " + str(computerNum))
 
-        answer = input("Do you want to play again? Y or N")
-        
-        #create a conditional to assess what happens if the player entered Y or N. 
+        answer = input("Do you want to play again? Y or N? ")
+
+        #create a conditional to assess what happens if the player entered Y or N.
+
+        if answer.upper() == "N":
+            print(answer)
+            print("Okay! byeee ")
+            still_playing = False
+        else:
+            win = False
+            tries = 5
+            computerNum = random.randint(0,20)
+                
 
 
 
